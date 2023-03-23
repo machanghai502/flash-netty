da2126a8819588549c42005fd9b2a9ec67595ac6
machanghai <machanghai502@163.com>
2023-03-23 22:33:32 +0800

    增加一个pipeline上所有的inboundhandler的生命周期方法调用，结果是每个方法都会一次按照pipeline的顺序依次调用。

--- src/main/java/the/flash/server/handler/LifeCyCleTestHandler.java
+++ src/main/java/the/flash/server/handler/LifeCyCleTestHandler.java
@@ -6,50 +6,50 @@ import io.netty.channel.ChannelInboundHandlerAdapter;
 public class LifeCyCleTestHandler extends ChannelInboundHandlerAdapter {
     @Override
     public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("逻辑处理器被添加：handlerAdded()");
+        System.out.println(this.getClass().getName() + "逻辑处理器被添加：handlerAdded()");
         super.handlerAdded(ctx);
     }
 
     @Override
     public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("channel 绑定到线程(NioEventLoop)：channelRegistered()");
+        System.out.println(this.getClass().getName() + "channel 绑定到线程(NioEventLoop)：channelRegistered()");
         super.channelRegistered(ctx);
     }
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("channel 准备就绪：channelActive()");
+        System.out.println(this.getClass().getName() + "channel 准备就绪：channelActive()");
         super.channelActive(ctx);
     }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        System.out.println("channel 有数据可读：channelRead()");
+        System.out.println(this.getClass().getName() + "channel 有数据可读：channelRead()");
         super.channelRead(ctx, msg);
     }
 
     @Override
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("channel 某次数据读完：channelReadComplete()");
+        System.out.println(this.getClass().getName() + "channel 某次数据读完：channelReadComplete()");
         super.channelReadComplete(ctx);
 
     }
 
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("channel 被关闭：channelInactive()");
+        System.out.println(this.getClass().getName() + "channel 被关闭：channelInactive()");
         super.channelInactive(ctx);
     }
 
     @Override
     public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("channel 取消线程(NioEventLoop) 的绑定: channelUnregistered()");
+        System.out.println(this.getClass().getName() + "channel 取消线程(NioEventLoop) 的绑定: channelUnregistered()");
         super.channelUnregistered(ctx);
     }
 
     @Override
     public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
-        System.out.println("逻辑处理器被移除：handlerRemoved()");
+        System.out.println(this.getClass().getName() + "逻辑处理器被移除：handlerRemoved()");
         super.handlerRemoved(ctx);
     }
 }
--- src/main/java/the/flash/server/handler/LoginRequestHandler.java
+++ src/main/java/the/flash/server/handler/LoginRequestHandler.java
@@ -30,4 +30,53 @@ public class LoginRequestHandler extends SimpleChannelInboundHandler<LoginReques
     private boolean valid(LoginRequestPacket loginRequestPacket) {
         return true;
     }
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "逻辑处理器被添加：handlerAdded()");
+        super.handlerAdded(ctx);
+    }
+
+    @Override
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "channel 绑定到线程(NioEventLoop)：channelRegistered()");
+        super.channelRegistered(ctx);
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "channel 准备就绪：channelActive()");
+        super.channelActive(ctx);
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        System.out.println(this.getClass().getName() + "channel 有数据可读：channelRead()");
+        super.channelRead(ctx, msg);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "channel 某次数据读完：channelReadComplete()");
+        super.channelReadComplete(ctx);
+
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "channel 被关闭：channelInactive()");
+        super.channelInactive(ctx);
+    }
+
+    @Override
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "channel 取消线程(NioEventLoop) 的绑定: channelUnregistered()");
+        super.channelUnregistered(ctx);
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+        System.out.println(this.getClass().getName() + "逻辑处理器被移除：handlerRemoved()");
+        super.handlerRemoved(ctx);
+    }
 }
